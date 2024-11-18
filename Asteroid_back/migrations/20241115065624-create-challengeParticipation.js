94ee05cf4081351f005493cbda732f385ea81c62
est22 <anlia.seoul@gmail.com>
2024-11-18 21:51:45 +0900

    fix: modify table name

--- Asteroid_back/migrations/20241115065624-create-challengeParticipation.js
+++ Asteroid_back/migrations/20241115065624-create-challengeParticipation.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("ChallengeMembers", {
+    await queryInterface.createTable("ChallengeParticipations", {
       id: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -58,6 +58,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("ChallengeMembers");
+    await queryInterface.dropTable("ChallengeParticipations");
   },
 };
