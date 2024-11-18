a559b07e2bfa45c05c57b70b899ea56445a073ce
est22 <anlia.seoul@gmail.com>
2024-11-18 21:53:15 +0900

    fix: correct field name at Comment table (comment->"content")
    자연스레 해버림

--- Asteroid_back/seeders/20241116082857-demo-comment.js
+++ Asteroid_back/seeders/20241116082857-demo-comment.js
@@ -5,7 +5,7 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert("Comments", [
       {
-        comment: "좋은 지출에 대해 칭찬합니다! 저도 이렇게 해보고 싶네요.",
+        content: "좋은 지출에 대해 칭찬합니다! 저도 이렇게 해보고 싶네요.",
         created_at: new Date(),
         user_id: 2,
         post_id: 1,
@@ -14,7 +14,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        comment: "소소한 성과지만 멋져요! 저도 운동을 시작해야겠어요.",
+        content: "소소한 성과지만 멋져요! 저도 운동을 시작해야겠어요.",
         created_at: new Date(),
         user_id: 3,
         post_id: 1,
@@ -23,7 +23,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        comment: "오늘 하루도 정말 힘드셨군요. 모두가 이런 날이 있죠.",
+        content: "오늘 하루도 정말 힘드셨군요. 모두가 이런 날이 있죠.",
         created_at: new Date(),
         user_id: 4,
         post_id: 3,
@@ -32,7 +32,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        comment: "저도 아침에 명상하는데, 점점 편안해져요. 힘내세요!",
+        content: "저도 아침에 명상하는데, 점점 편안해져요. 힘내세요!",
         created_at: new Date(),
         user_id: 2,
         post_id: 2,
@@ -41,7 +41,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        comment: "팀 프로젝트가 잘 끝나서 기쁩니다. 모두가 대단해요!",
+        content: "팀 프로젝트가 잘 끝나서 기쁩니다. 모두가 대단해요!",
         created_at: new Date(),
         user_id: 4,
         post_id: 2,
@@ -50,7 +50,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        comment: "이번 주 추천작 정말 궁금하네요! 꼭 보겠습니다.",
+        content: "이번 주 추천작 정말 궁금하네요! 꼭 보겠습니다.",
         created_at: new Date(),
         user_id: 2,
         post_id: 4,
