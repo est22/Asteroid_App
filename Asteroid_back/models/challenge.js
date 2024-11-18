b7c803254dc94b73fe36881fe448e1fd6cbaef7e
HONG-EUN-YOUNG <dms084@outlook.com>
2024-11-18 15:43:56 +0900

    edit db structure

--- Asteroid_back/models/challenge.js
+++ Asteroid_back/models/challenge.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
   class Challenge extends Model {
     static associate(models) {
       Challenge.hasMany(models.ChallengeParticipation, {
+      Challenge.hasMany(models.ChallengeParticipation, {
         foreignKey: "challenge_id",
       });
       Challenge.hasMany(models.Reward, { foreignKey: "challenge_id" });
@@ -14,11 +15,12 @@ module.exports = (sequelize, DataTypes) => {
   Challenge.init(
     {
       period: DataTypes.INTEGER,
+      period: DataTypes.INTEGER,
       name: DataTypes.STRING,
       description: DataTypes.STRING,
       reward_name: DataTypes.STRING,
       reward_image_url: DataTypes.STRING,
-      status: DataTypes.STRING,
+      status: DataTypes.CHAR(1),
     },
     {
       sequelize,
