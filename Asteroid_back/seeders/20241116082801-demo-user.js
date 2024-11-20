877a414c88ea6c0cd469e9907d2d451946d1dcf3
est22 <anlia.seoul@gmail.com>
2024-11-20 10:54:32 +0900

    fix DB field name and relations

--- Asteroid_back/seeders/20241116082801-demo-user.js
+++ Asteroid_back/seeders/20241116082801-demo-user.js
@@ -11,7 +11,7 @@ module.exports = {
           password: "admin",
           nickname: "관리자",
           motto: "나 관리자",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -19,7 +19,7 @@ module.exports = {
           password: "password",
           nickname: "갓흥민",
           motto: "절약도 예술임",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -27,7 +27,7 @@ module.exports = {
           password: "password",
           nickname: "짠테크왕",
           motto: "오늘도 커피값 아낌",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -35,7 +35,7 @@ module.exports = {
           password: "password",
           nickname: "김플렉스",
           motto: "작은 사치부터 시작",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -43,7 +43,7 @@ module.exports = {
           password: "password",
           nickname: "재벌꿈나무",
           motto: "소비는 전략이다",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -51,7 +51,7 @@ module.exports = {
           password: "password",
           nickname: "돌멩이",
           motto: "나는 나다",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -59,7 +59,7 @@ module.exports = {
           password: "password",
           nickname: "동그라미",
           motto: "심플 이즈 베스트",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -67,7 +67,7 @@ module.exports = {
           password: "password",
           nickname: "바다거북",
           motto: "천천히, 꾸준히",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -75,7 +75,7 @@ module.exports = {
           password: "password",
           nickname: "돈길만걷자",
           motto: "티끌 모아 플렉스",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -83,7 +83,7 @@ module.exports = {
           password: "password",
           nickname: "구름이",
           motto: "늘 자유롭게",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -91,7 +91,7 @@ module.exports = {
           password: "password",
           nickname: "램프요정",
           motto: "너의 소원을 말해봐",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -99,7 +99,7 @@ module.exports = {
           password: "password",
           nickname: "무지개",
           motto: "빛나는 삶을 꿈꾼다",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -107,7 +107,7 @@ module.exports = {
           password: "password",
           nickname: "짠돌잉",
           motto: "짠테크는 멋짐",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -115,7 +115,7 @@ module.exports = {
           password: "password",
           nickname: "푸른심장",
           motto: "바다를 닮은 마음",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -123,7 +123,7 @@ module.exports = {
           password: "password",
           nickname: "런닝맨",
           motto: "달리는 자가 승리한다",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -131,7 +131,7 @@ module.exports = {
           password: "password",
           nickname: "비트메이커",
           motto: "리듬 속에 살다",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -139,7 +139,7 @@ module.exports = {
           password: "password",
           nickname: "바람돌이",
           motto: "마음 가는 대로",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -147,7 +147,7 @@ module.exports = {
           password: "password",
           nickname: "허니버터칩",
           motto: "달콤한 인생",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -155,7 +155,7 @@ module.exports = {
           password: "password",
           nickname: "우주인",
           motto: "나만의 별을 찾아서",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -163,7 +163,7 @@ module.exports = {
           password: "password",
           nickname: "민들레",
           motto: "작지만 강하게",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -171,7 +171,7 @@ module.exports = {
           password: "password",
           nickname: "지브라",
           motto: "모든 길을 개척한다",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -179,7 +179,7 @@ module.exports = {
           password: "password",
           nickname: "탐험가",
           motto: "새로운 것을 두려워하지 않기",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -187,7 +187,7 @@ module.exports = {
           password: "password",
           nickname: "몽상가",
           motto: "꿈은 이루어진다",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -195,7 +195,7 @@ module.exports = {
           password: "password",
           nickname: "고양이",
           motto: "느긋하게 살자",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -203,7 +203,7 @@ module.exports = {
           password: "password",
           nickname: "판다링",
           motto: "달콤한 나태함",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -211,7 +211,7 @@ module.exports = {
           password: "password",
           nickname: "곰돌이",
           motto: "꾸준한 삶",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -219,7 +219,7 @@ module.exports = {
           password: "password",
           nickname: "오리날다",
           motto: "한계를 넘어서",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -227,7 +227,7 @@ module.exports = {
           password: "password",
           nickname: "복숭아",
           motto: "부드럽게, 확실하게",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -235,7 +235,7 @@ module.exports = {
           password: "password",
           nickname: "호랑이",
           motto: "기백으로 살아가자",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -243,7 +243,7 @@ module.exports = {
           password: "password",
           nickname: "토끼달림",
           motto: "빠르게, 신중하게",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -251,7 +251,7 @@ module.exports = {
           password: "password",
           nickname: "여우눈빛",
           motto: "날카로운 관찰자",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -259,7 +259,7 @@ module.exports = {
           password: "password",
           nickname: "한강물",
           motto: "흘러가자",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -267,7 +267,7 @@ module.exports = {
           password: "password",
           nickname: "바람숲",
           motto: "자연과 함께",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -275,7 +275,7 @@ module.exports = {
           password: "password",
           nickname: "불꽃놀이",
           motto: "순간을 즐기다",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -283,7 +283,7 @@ module.exports = {
           password: "password",
           nickname: "안개숲",
           motto: "신비로운 삶",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
@@ -291,7 +291,7 @@ module.exports = {
           password: "password",
           nickname: "장미가시",
           motto: "강인함을 품다",
-          created_at: new Date(),
+          createdAt: new Date(),
           updatedAt: new Date(),
         },
       ],
