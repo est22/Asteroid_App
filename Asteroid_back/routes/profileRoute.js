93079539a73cf4c04281e8e3531114a8ef3e8dfe
est22 <anlia.seoul@gmail.com>
2024-11-22 14:47:34 +0900

    fix: delete /check-nickname endpoint (resolve error)

--- Asteroid_back/routes/profileRoute.js
+++ Asteroid_back/routes/profileRoute.js
@@ -5,8 +5,6 @@ const { authenticateToken } = require("../middleware/auth_middleware");
 
 const router = express.Router();
 
-// 닉네임 중복 검사
-router.post("/check-nickname", authenticateToken, checkNickname);
 // 닉네임 및 좌우명 업데이트
 router.post("/update-profile", authenticateToken, updateProfile);
 // 프로필 사진 업로드
