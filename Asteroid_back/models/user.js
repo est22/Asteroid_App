d8298210717ac770d1ab477362ab480979d9c9eb
est22 <anlia.seoul@gmail.com>
2024-11-27 23:45:23 +0900

    feat: Add auto User un-suspension after 3 days

--- Asteroid_back/models/user.js
+++ Asteroid_back/models/user.js
@@ -53,8 +53,6 @@ module.exports = (sequelize, DataTypes) => {
       timestamps: true,
     }
   );
-
-  // afterUpdate 훅 추가
 User.addHook("afterUpdate", async (user, options) => {
   if (user.status === "S") {
     const Post = sequelize.models.Post;
@@ -69,19 +67,33 @@ User.addHook("afterUpdate", async (user, options) => {
 
     // (2) 관련 Comment의 isShow를 false로 변경
     await Comment.update(
-      { isShow: false }, 
+      { isShow: false },
       { where: { user_id: user.id } } // 해당 유저의 모든 댓글
     );
 
     // (3) 관련 BalanceVote의 isShow를 false로 변경
     await BalanceVote.update(
-      { isShow: false }, 
+      { isShow: false },
       { where: { user_id: user.id } } // 해당 유저의 모든 Vote
     );
 
     console.log(
       `User ${user.id}의 상태 변경으로 해당 게시글과 댓글이 숨겨졌습니다.`
     );
+
+    // (4) 정지 상태로 변경된 유저의 상태 변경 시간을 기록한 후,
+    // 3일 후에 자동으로 'A'로 복구
+    const updateTime = user.updatedAt;
+
+    // 3일 후 계산
+    const threeDaysLater = new Date(updateTime.getTime() + 3 * 24 * 60 * 60 * 1000); // 3일 후의 시간
+
+    // 현재 시간이 3일 후가 지나면 상태를 'A'로 변경
+    if (new Date() >= threeDaysLater) {
+      user.status = "A";
+      await user.save(); // 상태 변경을 DB에 반영
+      console.log(`User ${user.id}의 상태가 자동으로 복구되었습니다.`);
+    }
   }
 });
 
