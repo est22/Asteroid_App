ed2524c6c09eb54b66018af5e1760f74659800bb
est22 <anlia.seoul@gmail.com>
2024-11-28 20:21:18 +0900

    feat: Push notification to user when they got unsuspended

--- Asteroid_back/models/user.js
+++ Asteroid_back/models/user.js
@@ -46,6 +46,10 @@ module.exports = (sequelize, DataTypes) => {
         allowNull: true,
         defaultValue: "A", // A: Active, S: Suspended, D:Deactivated
       },
+      device_token: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
     },
     {
       sequelize,
@@ -55,44 +59,23 @@ module.exports = (sequelize, DataTypes) => {
   );
 User.addHook("afterUpdate", async (user, options) => {
   if (user.status === "S") {
-    const Post = sequelize.models.Post;
-    const Comment = sequelize.models.Comment;
-    const BalanceVote = sequelize.models.BalanceVote;
-
-    // (1) 관련 Post의 isShow를 false로 변경
-    await Post.update(
-      { isShow: false },
-      { where: { user_id: user.id } } // 해당 유저의 모든 게시글
-    );
-
-    // (2) 관련 Comment의 isShow를 false로 변경
-    await Comment.update(
-      { isShow: false },
-      { where: { user_id: user.id } } // 해당 유저의 모든 댓글
-    );
-
-    // (3) 관련 BalanceVote의 isShow를 false로 변경
-    await BalanceVote.update(
-      { isShow: false },
-      { where: { user_id: user.id } } // 해당 유저의 모든 Vote
-    );
-
-    console.log(
-      `User ${user.id}의 상태 변경으로 해당 게시글과 댓글이 숨겨졌습니다.`
-    );
-
-    // (4) 정지 상태로 변경된 유저의 상태 변경 시간을 기록한 후,
-    // 3일 후에 자동으로 'A'로 복구
     const updateTime = user.updatedAt;
+    const threeDaysLater = new Date(updateTime.getTime() + 3 * 24 * 60 * 60 * 1000);
 
-    // 3일 후 계산
-    const threeDaysLater = new Date(updateTime.getTime() + 3 * 24 * 60 * 60 * 1000); // 3일 후의 시간
-
-    // 현재 시간이 3일 후가 지나면 상태를 'A'로 변경
     if (new Date() >= threeDaysLater) {
       user.status = "A";
-      await user.save(); // 상태 변경을 DB에 반영
-      console.log(`User ${user.id}의 상태가 자동으로 복구되었습니다.`);
+      await user.save();
+      
+      // Push Notification 발송
+      await sendPushNotification({
+        deviceToken: user.device_token, // User 모델에 device_token 필드 추가 필요
+        title: "계정 상태 알림",
+        body: "계정 정지가 해제되었습니다.",
+        data: {
+          type: "suspension_lifted",
+          userId: user.id
+        }
+      });
     }
   }
 });
