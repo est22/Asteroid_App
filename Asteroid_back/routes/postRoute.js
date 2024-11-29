b1510243c849f5ea9cf9fa148e7624f91447cb1b
HONG-EUN-YOUNG <dms084@outlook.com>
2024-11-29 12:42:47 +0900

    feat: implement image saving for post CRUD operations

--- Asteroid_back/routes/postRoute.js
+++ Asteroid_back/routes/postRoute.js
@@ -1,14 +1,25 @@
 const express = require("express");
 const controller = require("../controllers/postController");
 const { authenticateToken } = require("../middleware/auth_middleware");
+const upload = require("../middleware/fileUpload_middleware"); // multer 설정 파일 import
 const router = express.Router();
 
 router.get("/hot", controller.hotPost);
 router.get("/", controller.findAllPost);
 router.get("/:id", controller.findPostById);
-router.post("/", controller.createPost);
-router.put("/:id", controller.updatePost);
-router.delete("/:id", controller.deletePost);
+router.post(
+  "/",
+  authenticateToken,
+  upload.array("images", 5),
+  controller.createPost
+);
+router.put(
+  "/:id",
+  authenticateToken,
+  upload.array("images", 5),
+  controller.updatePost
+);
+router.delete("/:id", authenticateToken, controller.deletePost);
 router.post("/:id/like", authenticateToken, controller.likePost);
 
 module.exports = router;
