4cd1d71856dddb65670e3eb546a490f0323489f0
est22 <anlia.seoul@gmail.com>
2024-12-03 11:42:16 +0900

    Squashed commit of the following:
    commit 5baffa55ac27503e24fc6bc2335245abf2e38564
    Author: est22 <anlia.seoul@gmail.com>
    Date:   Tue Nov 19 11:12:22 2024 +0900
    
        delete old project
    
    commit 27b66005ac814b2af6e75c8e41af88619fd84e9d
    Author: est22 <anlia.seoul@gmail.com>
    Date:   Tue Nov 19 11:11:27 2024 +0900
    
        init: create new project file for frontend
    
    commit 1ed3640f87074eda2fd1175d9d07dc5f35b352a3
    Author: est22 <anlia.seoul@gmail.com>
    Date:   Tue Nov 19 10:17:22 2024 +0900
    
        update settings

---
+++ Asteroid_front/Asteroid_front/Source/User Interface/Asteroid_frontApp.swift
@@ -0,0 +1,17 @@
+//
+//  Asteroid_frontApp.swift
+//  Asteroid_front
+//
+//  Created by Lia An on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct Asteroid_frontApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Asteroid_front/Asteroid_front/Source/User Interface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Asteroid_front
+//
+//  Created by Lia An on 11/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("소행성").font(.starFontB(size: 24))
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
