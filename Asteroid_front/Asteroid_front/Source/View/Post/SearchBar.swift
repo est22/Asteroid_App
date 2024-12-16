2ffecca1fd9f0da6bc2f72a599e0ed2a4a26c223
est22 <anlia.seoul@gmail.com>
2024-12-16 16:24:44 +0900

    design: search bar design, Search at Enter

--- Asteroid_front/Asteroid_front/Source/View/Post/SearchBar.swift
+++ Asteroid_front/Asteroid_front/Source/View/Post/SearchBar.swift
@@ -1,16 +1,17 @@
 import SwiftUI
 
 struct SearchBar: View {
-    @Binding var searchText:String
+    @Binding var searchText: String
     @State var isEditing = false
-    var handler:()->Void
+    var handler: () -> Void
     
     var body: some View {
         HStack {
-            TextField("title, content", text: $searchText)
-                .padding()
+            TextField("제목, 내용으로 검색", text: $searchText)
+                .padding(.horizontal, 12)
+                .padding(.vertical, 8)
                 .background(Color(.systemGray5))
-                .clipShape(.rect(cornerRadius: 50))
+                .clipShape(.rect(cornerRadius: 12))
                 .padding(.horizontal)
                 .onSubmit {
                     handler()
@@ -25,7 +26,8 @@ struct SearchBar: View {
                     isEditing = false
                     self.searchText = ""
                 } label: {
-                    Text("cancel")
+                    Text("취소")
+                        .foregroundColor(.primary)
                 }
                 .padding(.trailing, 10)
                 .transition(.identity)
